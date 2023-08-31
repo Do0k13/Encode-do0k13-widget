01c76910cde85fbaa6d1074409c32c48f52f0a2d
do0k13 <ludim.chavez@protonmail.com>
2023-08-30 22:17:28 -0600

    Update Chat-widgetv1.jsx

--- widget/Chat-widgetv1.jsx
+++ widget/Chat-widgetv1.jsx
@@ -3,7 +3,55 @@
 //Contract foked from https://gist.github.com/encody/bd4f10aa446e8002386e973200cdf6b9
 const contract =
   "ece61a112fbb05b5ff96fd4d63cb259c4bae966477829666d46ddc4e5121d801";
+//start Near Staking Const
+const Stake = styled.div`
+  .headTab {
+    width: 100%;
+    display: flex;
+    margin-bottom: 40px;
+  }
+`;
 
+const Tab = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const { hideBanner } = props;
+
+const Wrap = styled.div`
+  display: flex;
+  gap: 24px;
+  flex-wrap: wrap;
+`;
+const SummaryWrapper = styled.div`
+  margin-bottom: ${!!hideBanner ? "50px" : ""};
+`;
+const Title = styled.h1`
+  font-size: ${(p) => sizes[p.size].title};
+  color: #ffffff;
+  font-weight: 700;
+  margin: 0;
+
+  @media (max-width: 770px) {
+    font-size: 16px;
+    margin: 0;
+  }
+`;
+
+const { tabName } = state;
+State.init({
+  tabName: "stake", // stake | unstake,
+  nearBalance: "",
+});
+
+const updateTabName = (tabName) =>
+  State.update({
+    tabName,
+  });
+
+//Ends Near Staking Const
 //Get messages from the contract defining a number of messages
 const messages = Near.view(contract, "get_messages", { limit: 30 });
 
@@ -134,5 +182,41 @@ return (
       />
       <MessageButton onClick={sendMessage}>Send</MessageButton>
     </SendMsgCnt>
+    <Stake>
+      <div class="headTab">
+        <Title>Staking</Title>
+        <Tab>
+          <Widget
+            src={`ref-admin.near/widget/LiNEAR.Tab`}
+            props={{
+              updateTabName,
+              tabName: tabName,
+            }}
+          ></Widget>
+        </Tab>
+      </div>
+      <Wrap>
+        <Widget
+          src="ref-admin.near/widget/LiNEAR"
+          props={{
+            updateTabName,
+            tabName: tabName,
+          }}
+        ></Widget>
+        <Widget
+          src="ref-admin.near/widget/XREF"
+          props={{
+            updateTabName,
+            tabName: tabName,
+          }}
+        ></Widget>
+        <Widget
+          src="ref-admin.near/widget/NearX"
+          props={{
+            tabName,
+          }}
+        ></Widget>
+      </Wrap>
+    </Stake>
   </Chat>
 );
